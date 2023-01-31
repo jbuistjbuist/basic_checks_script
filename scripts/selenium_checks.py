911c65460f6da8a286e341148047b234d39c3a64
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2023-01-31 02:08:06 -0500

    Create function to search emails and return checks info

--- scripts/selenium_checks.py
+++ scripts/selenium_checks.py
@@ -1,12 +1,12 @@
 import os
 
-#import the classes defined in the classes file
+# import the classes defined in the classes file
 from classes import *
 
-#for debugging (printing complete objects to console)
+# for debugging (printing complete objects to console)
 from pprint import pprint
 
-#selenium modules
+# selenium modules
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -14,21 +14,27 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 import time
 
-#import dotenv module to use env variables
+# import dotenv module to use env variables
 from dotenv import load_dotenv
 
 load_dotenv()
 
-#test data
-shipping_address = Address('157 power line ct', 'Hamilton', 'Ontario', 'L9B1N6', 'Jeremy', 'Buist')
-billing_address1 = Address('157 power line ct', 'Hamilton', 'Ontario', 'L9B1N6', 'Jeremy', 'Buist')
-billing_address2 = Address('3511 Rue Ethel', 'Verdun', 'Quebec', 'H4G1R9', 'Jeremy', 'Buist')
+# test data
+shipping_address = Address(
+    '157 power line ct', 'Hamilton', 'Ontario', 'L9B1N6', 'Jeremy', 'Buist')
+billing_address1 = Address(
+    '157 power line ct', 'Hamilton', 'Ontario', 'L9B1N6', 'Jeremy', 'Buist')
+billing_address2 = Address('3511 Rue Ethel', 'Verdun',
+                           'Quebec', 'H4G1R9', 'Jeremy', 'Buist')
 
-sa_equals_ba = Order('jbuistjbuist@gmail.com', shipping_address, billing_address1)
+sa_equals_ba = Order('jbuistjbuist@gmail.com',
+                     shipping_address, billing_address1)
 sa_not_ba = Order('jbuistjbuist@gmail.com', shipping_address, billing_address2)
-paypal_order = Order('jbuistjbuist@gmail.com', shipping_address, None, 'jeremy.j.buist@gmail.com', True)
+paypal_order = Order('jbuistjbuist@gmail.com', shipping_address,
+                     None, 'jeremy.j.buist@gmail.com', True)
 
 
 chrome_options = Options()
@@ -39,7 +45,7 @@ chrome_options.add_argument('--start-maximized')
 
 chrome_drive = Service()
 driver = webdriver.Chrome(service=chrome_drive, options=chrome_options)
-driver.implicitly_wait(50)
+driver.implicitly_wait(30)
 ekata_password = os.getenv("ekata_password")
 email = os.getenv("agent_email")
 
@@ -65,22 +71,59 @@ def initialize_webdriver():
 
 
 def get_ekata_info(order):
-  driver.switch_to.window(driver.window_handles[0])
+  driver.switch_to.window(ekata_window)
   app = driver.find_element(By.CLASS_NAME, 'css-1fe9b2j-ApplicationContainer')
-  app.find_element(By.XPATH, "//a[text()='Email']").click()
-  app.find_element(By.NAME, 'email').send_keys(order.member_email)
-  app.find_element(By.CLASS_NAME, 'css-uw1vjm-StyledButton-primary').click()
+
+  member_email_check = perform_email_checks(order.member_email, app)
+
+  if (order.paypal_email):
+    paypal_email_check = 'PayPal ' + perform_email_checks(order.paypal_email, app)
+    member_email_check = 'Member ' + member_email_check
+
+  else:
+    paypal_email_check = None
 
 
-  email_validity = driver.find_element(By.XPATH, '//div[2]/div[2]/div[2]').text
-  online_presence = driver.find_element(By.XPATH, '//div[3]/div[2]').text
-  creation = driver.find_element(By.XPATH, '//div[4]/div[2]').text
-  domain_reputation = driver.find_element(By.XPATH, '//div[3]/div[4]/div[2]').text
+  print(member_email_check, paypal_email_check)
 
-  print(email_validity, online_presence, creation, domain_reputation)
+
+def perform_email_checks(email, dom_section):
+  driver.implicitly_wait(5)
+  dom_section.find_element(By.XPATH, "//a[text()='Email']").click()
+  email_input = dom_section.find_element(By.NAME, 'email')
+  email_input.send_keys(Keys.COMMAND, 'a')
+  email_input.send_keys(Keys.DELETE)
+  email_input.send_keys(email)
+  dom_section.find_element(
+      By.CLASS_NAME, 'css-uw1vjm-StyledButton-primary').click()
+
+  try:
+    email_validity = dom_section.find_element(By.XPATH, '//div[2]/div[2]/div[2]').text
+    online_presence_html = dom_section.find_element(By.XPATH, '//div[2]/div[3]')
+    online_presence = online_presence_html.find_element(By.CLASS_NAME, 'e8160634').text
+
+    try:
+      driver.implicitly_wait(0)
+      time_ago = online_presence_html.find_element(By.CLASS_NAME, 'e8160633').text
+      online_presence = online_presence + f' ({time_ago})'
+      driver.implicitly_wait(5)
+    except NoSuchElementException:
+      driver.implicitly_wait(5)
+    
+    creation = dom_section.find_element(By.XPATH, '//div[4]/div[2]').text
+    domain_reputation = dom_section.find_element(By.XPATH, '//div[3]/div[4]/div[2]').text
+
+    return f'Email - Validity: {email_validity}, Online Presence: {online_presence}, Creation: {creation}, Domain: {domain_reputation}'
+
+  except NoSuchElementException:
+    return 'Email - check Failed, please review manually'
+
+
 
 
 
 initialize_webdriver()
 
 get_ekata_info(sa_equals_ba)
+get_ekata_info(sa_not_ba)
+get_ekata_info(paypal_order)
